08b0e0abbf63194c742d63e5b70cbb4959cc64aa
kenny Egun <kennyegun240@gmail.com>
2022-10-28 15:05:31 +0100

    Add footer to app

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,9 +1,11 @@
-export default footer;
-const font = () =>{
-const footer = document.getElementById('footer');
+const font = () => {
+  const footer = document.getElementById('footer');
 
-footer.innerHTML += `<hr class="foot-line">
+  footer.innerHTML += `<hr class="foot-line">
 <p class="foot-item">Created by microverse under CC license</p>
 `;
-}
-font();
+};
+
+/* eslint-disable */
+export { font };
+/* eslint-enable */
