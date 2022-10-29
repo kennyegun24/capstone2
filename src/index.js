de2bba6e6da419c3d7def7f15493d3cfd89ea010
kenny Egun <kennyegun240@gmail.com>
2022-10-29 13:24:20 +0100

    Add item counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './style.css';
 import { display } from './modules/displayfood';
 import { font } from './modules/footer';
+import { foodApiObj } from './modules/APIs'
 
+const item_counter = document.querySelector('.item_count')
+
+// item_counter.addEventListener('click', () => {
+  
+// })
+
+item_counter.innerHTML = 'Total Items:' + foodApiObj.length
 display();
 font();
