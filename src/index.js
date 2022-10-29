d87772b97d8557442658b33f23f6a149b89eb8fe
kenny Egun <kennyegun240@gmail.com>
2022-10-29 13:36:07 +0100

    Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import './style.css';
 import { display } from './modules/displayfood';
 import { font } from './modules/footer';
-import { foodApiObj } from './modules/APIs'
+import { foodApiObj } from './modules/APIs';
 
-const item_counter = document.querySelector('.item_count')
+const itemCounter = document.querySelector('.item_count');
 
-// item_counter.addEventListener('click', () => {
-  
-// })
-
-item_counter.innerHTML = 'Total Items:' + foodApiObj.length
+itemCounter.innerHTML = `Total Items:${foodApiObj.length}`;
 display();
 font();
